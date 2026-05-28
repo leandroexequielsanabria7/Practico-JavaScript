76df29862f045fb718f0a785b40fdbe4e7eb9077
unknown <leandroexequielsanabria7@gmail.com>
2026-05-28 13:56:50 -0300

    corrijo actividad 1

---
+++ actividad1.js
@@ -0,0 +1,6 @@
+let nombre = "Leandro";
+let edad = 20;
+const carrera = "Tecnicatura en Desarrollo de Software Multiplataforma";
+const cursando = true;
+
+console.log(`Mi nombre es ${nombre}, tengo ${edad} años, estudio ${carrera} y estoy cursando: ${cursando}`);
